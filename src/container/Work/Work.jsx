f2fd8b5855e515a76a66ac4f6194a1f952b08513
Mohammad Mudassir <mdmudassir0143@gmail.com>
2023-02-10 10:04:45 +0530

    Dapp

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -41,7 +41,7 @@ const Work = () => {
       <h2 className="head-text">My Creative <span>Portfolio</span> Section</h2>
 
       <div className="app__work-filter">
-        {['UI/UX', 'Web App', 'Dapps', 'React JS', 'All'].map((item, index) => (
+        {['UI/UX', 'Web App', 'Dapp', 'React JS', 'All'].map((item, index) => (
           <div
             key={index}
             onClick={() => handleWorkFilter(item)}

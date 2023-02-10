c9d863f6ae606d6668e96abad36e36aed00510f2
Mohammad Mudassir <mdmudassir0143@gmail.com>
2023-02-10 10:58:08 +0530

    social media icons

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -5,13 +5,19 @@ import { FaGithub } from 'react-icons/fa';
 const SocialMedia = () => (
   <div className="app__social">
     <div>
-      <BsLinkedin />
+      <a href="https://www.linkedin.com/in/mohammad-mudassir-b788a41ab/" target="_blank" rel="noopener noreferrer">
+        <BsLinkedin />
+      </a>
     </div>
     <div>
-      <FaGithub />
+      <a href="https://github.com/mdmudassir0143" target="_blank" rel="noopener noreferrer">
+        <FaGithub />
+      </a>
     </div>
     <div>
-      <BsTwitter />
+      <a href="https://twitter.com/mdmudassir0143" target="_blank" rel="noopener noreferrer">
+        <BsTwitter />
+      </a>
     </div>
   </div>
 );
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -27,7 +27,7 @@ const Header = () => (
         <div className="badge-cmp app__flex">
           <span>👋</span>
           <div style={{ marginLeft: 20 }}>
-            <p className="p-text">Hello, I am</p>
+            <p className="p-text">Hello, I am Mohammad</p>
             <h1 className="head-text">Mudassir</h1>
           </div>
         </div>
